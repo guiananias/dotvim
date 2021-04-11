81ebd455965aaaf7f64c5355594c16e7196a9e7b
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-04-10 21:19:07 -0300

    add initial config of nvim-tree plugin

--- lua/plugins/editor/nvim-tree/init.lua
+++ lua/plugins/editor/nvim-tree/init.lua
@@ -1,1 +1,25 @@
-vim.api.nvim_set_keymap('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+Variable.g({
+	nvim_tree_side = 'left',
+	nvim_tree_width = 30,
+	nvim_tree_auto_open = 1,
+	nvim_tree_auto_close = 1,
+	nvim_tree_follow = 1,
+	nvim_tree_git_hl = 1,
+	nvim_tree_indent_markers = 1,
+	nvim_tree_ignore = {
+		'.git', 
+		'node_modules',
+		'.cache',
+	},
+	nvim_tree_show_icons = {
+		git = 1,
+		folders = 1,
+		files = 1
+	}
+})
+
+local opt = { noremap = true, silent = true }
+
+Keybind.g({
+	{ 'n', '<C-b>', ':NvimTreeToggle<CR>', opt },
+}) 
