373c33defd262844a84f76dbfac98af1266b859d
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-04-14 03:08:58 -0300

    add <C-Space> keybind to nvim-compe

--- lua/plugins/nvim-compe/init.lua
+++ lua/plugins/nvim-compe/init.lua
@@ -1,26 +1,31 @@
 require('compe').setup({
-  enabled = true;
-  autocomplete = true;
+  enabled = true,
+  autocomplete = true,
   debug = false,
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = false;
-
+  min_length = 1,
+  preselect = 'enable',
+  throttle_time = 80,
+  source_timeout = 200,
+  incomplete_delay = 400,
+  max_abbr_width = 100,
+  max_kind_width = 100,
+  max_menu_width = 100,
+  documentation = true,
   source = {
-    path = true;
-    buffer = true;
-    calc = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    spell = true;
-    tags = true;
-    treesitter = true;
+    path = true,
+    buffer = true,
+    calc = true,
+    nvim_lsp = true,
+    nvim_lua = true,
+    spell = true,
+    tags = true,
+    treesitter = true
   }
+})
+
+local opts = { noremap = true, silent = true, expr = true }
+
+Keybind.g({
+  { 'i', '<C-Space>', 'compe#complete()', opts },
 })
 
