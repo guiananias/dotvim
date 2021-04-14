e7609cb1e9ce2003b0422f437795120fb2365c01
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-04-14 03:06:40 -0300

    fix indent on plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,47 +11,47 @@ end
 execute 'autocmd BufWritePost plugins.lua PackerCompile'
 
 return require("packer").startup(function (use)
-	use 'wbthomason/packer.nvim'
+  use 'wbthomason/packer.nvim'
 
-	-- General plugins
-	use 'tpope/vim-sensible'
-	use 'tpope/vim-commentary'
-	use 'tpope/vim-surround'
-	use 'tpope/vim-dispatch'
-	use 'tpope/vim-fugitive'
-	use 'tpope/vim-rhubarb'
+  -- General plugins
+  use 'tpope/vim-sensible'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-dispatch'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-rhubarb'
   use { 'mg979/vim-visual-multi', branch = 'master' }
-	use 'iamcco/markdown-preview.nvim'
-	use 'kyazdani42/nvim-tree.lua'
-	use 'vimwiki/vimwiki'
-	use 'windwp/nvim-autopairs'
-	use 'glepnir/galaxyline.nvim'
+  use 'iamcco/markdown-preview.nvim'
+  use 'kyazdani42/nvim-tree.lua'
+  use 'vimwiki/vimwiki'
+  use 'windwp/nvim-autopairs'
+  use 'glepnir/galaxyline.nvim'
 
-	use {
-	  'nvim-telescope/telescope.nvim',
-	  requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' } 
-        }
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' }
+  }
 
-	use {
-   	  'lewis6991/gitsigns.nvim',
-	  requires = {
-            'nvim-lua/plenary.nvim'
-          }
-        }
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    }
+  }
 
-	use 'neovim/nvim-lspconfig'
-	use 'kabouzeid/nvim-lspinstall'
-	use 'hrsh7th/nvim-compe'
-	use 'hrsh7th/vim-vsnip'
+  use 'neovim/nvim-lspconfig'
+  use 'kabouzeid/nvim-lspinstall'
+  use 'hrsh7th/nvim-compe'
+  use 'hrsh7th/vim-vsnip'
 
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
-	use 'editorconfig/editorconfig-vim'
+  use 'editorconfig/editorconfig-vim'
 
-	use 'wakatime/vim-wakatime'
+  use 'wakatime/vim-wakatime'
 
-	use 'ryanoasis/vim-devicons'
-	use 'kyazdani42/nvim-web-devicons'
+  use 'ryanoasis/vim-devicons'
+  use 'kyazdani42/nvim-web-devicons'
 
-	use 'nanotech/jellybeans.vim' -- Theme
+  use 'nanotech/jellybeans.vim' -- Theme
 end)
