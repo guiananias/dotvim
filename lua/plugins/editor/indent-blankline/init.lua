adad0cc11a7578bb29ba4604edd368e063187df1
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-05-09 02:34:41 -0300

    add initial config file to indent-blankline.nvim

---
+++ lua/plugins/editor/indent-blankline/init.lua
@@ -0,0 +1,15 @@
+local g = vim.g
+local cmd = vim.cmd
+
+g.indentLine_enabled = 1;
+g.indent_blankline_char = "▏"
+
+
+g.indent_blankline_filetype_exclude = {"help", "terminal"}
+g.indent_blankline_buftype_exclude = {"terminal"}
+
+g.indent_blankline_show_trailing_blankline_indent = false
+g.indent_blankline_show_first_indent_level = false
+
+cmd "hi IndentBlanklineChar guifg=#2a2e36"
+
