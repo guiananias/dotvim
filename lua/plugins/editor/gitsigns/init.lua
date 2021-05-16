98cea6fae4f071f6c2b73273ce9533553d861888
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-05-15 22:25:31 -0300

    fix config to gitsigns

--- lua/plugins/editor/gitsigns/init.lua
+++ lua/plugins/editor/gitsigns/init.lua
@@ -1,1 +1,30 @@
-require 'gitsigns'.setup()
+require("gitsigns").setup {
+  signs = {
+    add = {hl = "DiffAdd", text = "▐", numhl = "GitSignsAddNr"},
+    change = {hl = "DiffChange", text = "▐", numhl = "GitSignsChangeNr"},
+    delete = {hl = "DiffDelete", text = "_", numhl = "GitSignsDeleteNr"},
+    topdelete = {hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr"},
+    changedelete = {hl = "DiffChange", text = "~", numhl = "GitSignsChangeNr"}
+  },
+  numhl = false,
+  keymaps = {
+    noremap = true,
+    buffer = true,
+    ["n ]c"] = {expr = true, '&diff ? \']c\' : \'<cmd>lua require"gitsigns".next_hunk()<CR>\''},
+    ["n [c"] = {expr = true, '&diff ? \'[c\' : \'<cmd>lua require"gitsigns".prev_hunk()<CR>\''},
+    ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line()<CR>'
+ },
+ watch_index = { interval = 100 },
+ sign_priority = 5,
+ status_formatter = nil -- Use default
+}
+
+local cmd = vim.cmd
+
+cmd "hi DiffAdd guifg = #2ac952 guibg = none"
+cmd "hi DiffChange guifg = #9c1b08 guibg = none"
+cmd "hi DiffModified guifg = #2ac952 guibg = none"
