0e415d0df2308b904376fb1ae9cbe447630efd50
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-05-20 04:51:33 -0300

    add a initial config to galaxyline with mode

--- lua/plugins/editor/galaxyline/init.lua
+++ lua/plugins/editor/galaxyline/init.lua
@@ -1,58 +1,46 @@
 local gl = require 'galaxyline'
-local gl_section = gl.section
 
-local COLORS = {
-  bg = '#292D38',
-  yellow = '#DCDCAA',
-  dark_yellow = '#D7BA7D',
-  cyan = '#4EC9B0',
-  green = '#608B4E',
-  light_green = '#B5CEA8',
-  string_orange = '#CE9178',
-  orange = '#FF8800',
-  purple = '#C586C0',
-  magenta = '#D16D9E',
-  grey = '#858585',
-  blue = '#569CD6',
-  vivid_blue = '#4FC1FF',
-  light_blue = '#9CDCFE',
-  red = '#D16969',
-  error_red = '#F44747',
-  info_yellow = '#FFCC66',
-  transparent = '#0000'
+local colors = {
+  bg = "#1e222a",
+  line_bg = "#1e222a",
+  fg = "#d8dee9",
+  green = "#bbe67e",
+  orange = "#ff8000",
+  red = "#df8890",
+  lightbg = "#282c34",
+  nord = "#81a1c1",
+  green_yellow = "#ebc888"
 }
 
-gl.short_line_list = { 'NvimTree', 'packer' }
+gl.short_line_list = {" "}
 
-gl_section.left[1] = {
+gl.section.left[1] = {
+  FileIcon = {
+    provider = "FileIcon",
+    condition = buffer_not_empty,
+    hightlight = {
+      require 'galaxyline.provider_fileinfo'.get_file_icon_color,
+      colors.lightbg
+    }
+  }
+}
+
+gl.section.left[2] = {
   ViMode = {
     provider = function()
-      local mode_color = {
-        n = COLORS.blue,
-        i = COLORS.green,
-        v = COLORS.purple,
-        [''] = COLORS.purple,
-        V = COLORS.purple,
-        c = COLORS.magenta,
-        no = COLORS.blue,
-        s = COLORS.orange,
-        S = COLORS.orange,
-        ic = COLORS.yellow,
-        R = COLORS.red,
-        Rv = COLORS.red,
-        cv = COLORS.blue,
-        ce = COLORS.blue,
-        r = COLORS.cyan,
-        rm = COLORS.cyan,
-        ['r?'] = COLORS.cyan,
-        ['!'] = COLORS.blue,
-        t = COLORS.blue
+      local alias = {
+        n = "NORMAL",
+        i = "INSERT",
+        c = "COMMAND",
+        v = "VISUAL",
+        V = "VISUAL",
+        [""] = "VISUAL",
+        R = "REPLACE"
       }
-
-      vim.api.nvim_command('hi GalaxyViMode guifg=' .. mode_color[vim.fn.mode()])
-      return '▊ '
+      return alias[vim.fn.mode()]
     end,
-    highlight = { COLORS.red, COLORS.transparent }
+    highlight = { colors.bg, colors.red },
+    separator_highlight = { colors.red, colors.red }
   }
 }
 
